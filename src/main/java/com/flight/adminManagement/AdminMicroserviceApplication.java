91280e354e4b64c05b2224906f41c10c74d97c69
Gani878 <reddyganesh878@gmail.com>
2024-07-29 23:10:04 +0530

    Update AdminMicroserviceApplication.java with eureka client annotation

--- src/main/java/com/flight/adminManagement/AdminMicroserviceApplication.java
+++ src/main/java/com/flight/adminManagement/AdminMicroserviceApplication.java
@@ -2,12 +2,23 @@ package com.flight.adminManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.client.RestTemplate;
+
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AdminMicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminMicroserviceApplication.class, args);
+	}
+
+    @LoadBalanced
+    @Bean
+    RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
